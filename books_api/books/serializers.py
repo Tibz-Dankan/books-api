45f8e4dfa4440b3bc1aec3b174c7399c8d75f837
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-07-30 00:02:42 +0300

    fix: convert the book's number of pages into an integer before creating

--- books_api/books/serializers.py
+++ books_api/books/serializers.py
@@ -5,3 +5,18 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+    def validate_pages(self, value):
+        try:
+            return int(value)
+        except ValueError:
+            raise serializers.ValidationError("Pages must be an integer.")
+
+    def create(self, validated_data):
+        validated_data['pages'] = int(validated_data.get('pages', 0))
+        print("validated_data['pages']: ",validated_data['pages'])
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        validated_data['pages'] = int(validated_data.get('pages', 0))
+        return super().update(instance, validated_data)
